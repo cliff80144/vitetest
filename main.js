9a67ce3bc2681cd1d3ef0c98c0a48c263d04e527
cliffwu <cliff104017023@gmail.com>
2024-10-29 14:26:30 +0800

    加fonts

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import './assets/scss/layout/bootstrap.min.css';
 import './assets/scss/layout/standard.scss';
 import './assets/scss/layout/_header.scss';
 import './assets/scss/layout/_footer.scss';
