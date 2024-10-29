9b8ed68ea1727af3575732a1ca7e6a556485e305
cliffwu <cliff104017023@gmail.com>
2024-10-29 14:04:44 +0800

    mya17

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-import './assets/scss/all.scss';
+import './assets/scss/layout/standard.scss';
+import './assets/scss/layout/_header.scss';
+import './assets/scss/layout/_footer.scss';
+import './assets/scss/story.scss';
 
 console.log("Hello world!");
